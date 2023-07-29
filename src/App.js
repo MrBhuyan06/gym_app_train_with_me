67dee571baf62fbf2546a566ca73e3700bb05138
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-07-29 23:54:04 +0530

    Added the routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Outlet } from "react-router-dom";
+import Nav from "./components/Nav.js";
+import { Box } from "@mui/material";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Nav />
+      <Outlet />
+    </Box>
   );
-}
-
+};
 export default App;
