9e7d0c565800f63289a00c41fcd179347a5fcffe
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-07-30 00:13:10 +0530

    Added the routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Nav from "./components/Nav.js";
+// import { Box } from "@mui/material";
 import { Box } from "@mui/material";
 const App = () => {
   return (
