2b580000c96ca0595b26f4f464c2da9799572926
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-07-30 21:21:20 +0530

    Added the pagination

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -5,7 +5,19 @@ import { fetchExerciseData, api_Options } from "../utils/constant.js";
 import ExerciseCard from "./ExerciseCard.js";
 
 const Exercises = ({ setExercise, exercise, bodyPart }) => {
-  console.log(exercise);
+  const [currentPage, setCurrentPage] = useState(1);
+  // console.log(exercise);
+  const [exePerPage] = useState(6);
+  //update the current page
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+    window.scrollTo({ top: 1800, behavior: "smooth" });
+  };
+  //pagination logic
+  const indexOfLastExe = currentPage * exePerPage;
+  const indexOfFirstExe = indexOfLastExe - exePerPage;
+  const numberCurrentExe = exercise.slice(indexOfFirstExe, indexOfLastExe);
+
   return (
     <Box id="exercises" sx={{ mt: { lg: "109px" } }} mt="50px" p="20px">
       <Typography
@@ -22,9 +34,26 @@ const Exercises = ({ setExercise, exercise, bodyPart }) => {
         flexWrap="wrap"
         justifyContent="center"
       >
-        {exercise.map((exe) => {
+        {numberCurrentExe.map((exe) => {
           return <ExerciseCard key={exe.id} data={exe} />;
         })}
+      </Stack>
+      <Stack
+        sx={{ mt: { lg: "114px", xs: "70px" }, color: "#fff" }}
+        alignItems="center"
+      >
+        {exercise.length > 9 && (
+          <Pagination
+            color="primary"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercise.length / exePerPage)}
+            page={currentPage}
+            onChange={paginate}
+            //bydefault=(e) => paginate(e,value)
+            size="large"
+          />
+        )}
       </Stack>
     </Box>
   );
